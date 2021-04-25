615b8f9edb7b2e2f4fec18326b2f9e9a6eb07ac3
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-04-26 00:08:09 +0200

    Aufgabe 3

--- Versuch_1/__init__.py
+++ Versuch_1/__init__.py
@@ -1,14 +1,69 @@
-import glob
-
-import matplotlib
 import matplotlib.pyplot as plt
-from numpy import *
 from pylab import *
-import os
+
+# 1. Ermittlung der Kennlinie des Abstandssensors
+
+#Erstellen Sie eine Python-Funktion, welche die Daten aus der Datei einliest (verwenden Sie
+#dazu die Numpy-Funktion genfromtxt()), von den Daten die ersten 1000 Werte überspringt
+#und aus den nächsten Werten (nehmen Sie eine sinnvolle Anzahl von Messungen) den
+#Mittelwert und die Standardabweichung berechnet (der Einschwingvorgang wird dadurch
+#ignoriert). Vergleichen Sie die Ergebnisse mit den Werten aus ihrem Messprotokoll.
+m1_data = np.genfromtxt('../messwerte/m1.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m2_data = np.genfromtxt('../messwerte/m2.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m3_data = np.genfromtxt('../messwerte/m3.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m4_data = np.genfromtxt('../messwerte/m4.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m5_data = np.genfromtxt('../messwerte/m5.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m6_data = np.genfromtxt('../messwerte/m6.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m7_data = np.genfromtxt('../messwerte/m7.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m8_data = np.genfromtxt('../messwerte/m8.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m9_data = np.genfromtxt('../messwerte/m9.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m10_data = np.genfromtxt('../messwerte/m10.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m11_data = np.genfromtxt('../messwerte/m11.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m12_data = np.genfromtxt('../messwerte/m12.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m13_data = np.genfromtxt('../messwerte/m13.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m14_data = np.genfromtxt('../messwerte/m14.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m15_data = np.genfromtxt('../messwerte/m15.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m16_data = np.genfromtxt('../messwerte/m16.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m17_data = np.genfromtxt('../messwerte/m17.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m18_data = np.genfromtxt('../messwerte/m18.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m19_data = np.genfromtxt('../messwerte/m19.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+m20_data = np.genfromtxt('../messwerte/m20.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
+
+file_mean = open("mean_data.txt", "w+")
+file_std = open("std_data.txt", "w+")
+data_list = [m1_data, m2_data, m3_data, m4_data, m5_data, m6_data, m7_data,
+             m8_data, m9_data, m10_data, m11_data, m12_data, m13_data, m14_data,
+             m15_data, m16_data, m17_data, m18_data, m19_data, m20_data]
+data_means = []
+data_std = []
+messdistanzmm = [100, 130, 160, 190, 220, 250, 280, 310, 330, 360, 390, 420, 450, 480, 510, 540, 570, 600, 630, 660]
+messdistanzcm = [10, 13, 16, 19, 22, 25, 28, 31, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66]
+count = 0;
+
+for i in data_list:
+    loc_data_mean = np.mean(i, dtype=float)
+    loc_data_std = np.std(i, dtype=float)
+    count = count + 1
+    file_mean.write("%d: Mean: %1.5f |" % (count, loc_data_mean))
+    file_mean.write(" STD: %f\n" % loc_data_std)
+    data_means.append(loc_data_mean)
+    data_std.append(loc_data_std)
 
 
-#AUFGABE 1
+#Stellen Sie die gefundene Übertragungsfunktion bzw. Kennlinie im Protokoll graphisch mit Python
+#bzw. Matplotlib dar
 
+fig, ax = plt.subplots()
+
+ax.plot(messdistanzcm[:], data_means[:])
+ax.set_xlabel('Abstand [in cm]')
+ax.set_ylabel('Spannung [in V]')
+ax.set_title('Kennlinien');
+show()
+
+
+#2. Modellierung der Kennlinie durch lineare Regression
+#Discord Werte 'a1.pdf'
 wertenichtflip = np.array([[69, 0.3754],
               [66, 0.3799],
               [63, 0.3844],
@@ -32,113 +87,42 @@ wertenichtflip = np.array([[69, 0.3754],
 
 werte = np.flip(wertenichtflip) #Flippen, da der Dude ders erstellt hat falsch rum gemessen hat.
 
-fig, ax = plt.subplots()
+
+#2.1. Logarithmieren Sie zunächst alle (Spannung)Eingangs- und (Entfernung)Ausgangswerte in der Tabelle und
+#stellen den Zusammenhang graphisch dar.
 werte_x = werte[:,0]
 werte_y = werte[:,1]
-#werte[:,1] = x
-#werte[:,0] = y
-ax.plot(werte_y, werte_x) # Aufgabe 1 x = Abstand, y = Spannung , NUR BEI AUFGABE 1
-ax.set_xlabel('Abstand [in cm]')
-ax.set_ylabel('Spannung [in mV]')
-ax.set_title('a1.pdf Kennlinie');
-show()
+logawerteEingang = np.log(werte_x) #Spannung Volt
+logawerteAusgang = np.log(werte_y) #Abstand cm
 
 
-#Daten einlesen (Moodle Daten) Hieraus Funktion machen!
-m1_data = np.genfromtxt('../messwerte/m1.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m2_data = np.genfromtxt('../messwerte/m2.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m3_data = np.genfromtxt('../messwerte/m3.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m4_data = np.genfromtxt('../messwerte/m4.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m5_data = np.genfromtxt('../messwerte/m5.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m6_data = np.genfromtxt('../messwerte/m6.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m7_data = np.genfromtxt('../messwerte/m7.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m8_data = np.genfromtxt('../messwerte/m8.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m9_data = np.genfromtxt('../messwerte/m9.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m10_data = np.genfromtxt('../messwerte/m10.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m11_data = np.genfromtxt('../messwerte/m11.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m12_data = np.genfromtxt('../messwerte/m12.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m13_data = np.genfromtxt('../messwerte/m13.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m14_data = np.genfromtxt('../messwerte/m14.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m15_data = np.genfromtxt('../messwerte/m15.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m16_data = np.genfromtxt('../messwerte/m16.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m17_data = np.genfromtxt('../messwerte/m17.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m18_data = np.genfromtxt('../messwerte/m18.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m19_data = np.genfromtxt('../messwerte/m19.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
-m20_data = np.genfromtxt('../messwerte/m20.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
 
-data_list = [m1_data, m2_data, m3_data, m4_data, m5_data, m6_data, m7_data,
-             m8_data, m9_data, m10_data, m11_data, m12_data, m13_data, m14_data,
-             m15_data, m16_data, m17_data, m18_data, m19_data, m20_data]
-
-file_mean = open("mean_data.txt", "w+")
-file_std = open("std_data.txt", "w+")
-print("start saving Data ...")
-count = 0;
-#Hier kann man (oder drüber) die data_list und messdistanz in eine liste packen, mit zugehörigen tupeln z.b., oder wie oben (ARRAY/ARRAY)
-data_means = []
-messdistanz = [100, 130, 160, 190, 220, 250, 280, 310, 330, 360, 390, 420, 450, 480, 510, 540, 570, 600, 630, 660]
-
-#plot
-for i in data_list:
-    loc_data_mean = np.mean(i, dtype=float)
-    loc_data_std = np.std(i, dtype=float)
-    count = count + 1
-    file_mean.write("%d: Mean: %1.5f |" % (count, loc_data_mean))
-    file_mean.write(" STD: %f\n" % loc_data_std)
-    data_means.append(loc_data_mean)
-
-print("Data saved")
-#plot
-#fig, ax = plt.subplots()
-
-#ax.plot(messdistanz[:], data_means[:])
-#ax.set_xlabel('Abstand [in mm]')
-#ax.set_ylabel('Spannung [in mV]')
-#ax.set_title('Kennlinien');
-#show()
-
-# Aufgabe 2. Aufgabe 1 x = Spannung, y = Abstand
-# Nummer 1 Logarithmieren
-logawerteEingang = np.log(werte_x)
-logawerteAusgang = np.log(werte_y)
-
-# Nummer 2 Neue Kennlinie
-logawerte = np.log(werte)
-
-#plot
+#2.2. Der resultierende Kennlinie sollte die Form einer Geraden haben (Plotten)
 fig, ax = plt.subplots()
 
-ax.plot(logawerteEingang, logawerteAusgang) # logawerte[:,1] = x, logawerte[:,0] = y
+ax.plot(logawerteEingang, logawerteAusgang)
 ax.set_xlabel('Log Spannung [in V]')
 ax.set_ylabel('Log Abstand [in cm]')
-ax.set_title('a1.pdf Kennlinie');
+ax.set_title('a1.pdf Kennlinie Logarithmiert');
 show()
 
 
-excellogawerteEingang = np.log(messdistanz)
-excellogawerteAusgang = np.log(data_means)
-excelx = excellogawerteEingang[:]
-excely = excellogawerteAusgang[:]
-(excela, excelb) = np.polyfit(excelx, excely, 1)
-excelyp = np.polyval([excela, excelb], excelx)
-print("EXCEL: Nichtlineare Kennlinie: y = e^%.3f * x^%.3f" % (excela, excelb))
-#excelnewY = pow(e, excelb) * pow(messdistanz, excela) #Rückrechnung
+#2.3. Berechnen Sie mithilfe der linearen Regression, wie in der Vorlesung behandelt, die
+#Ausgleichsgerade in Python. Eliminieren Sie ggf. Werte für sehr große Entfernungen,
+#für die der lineare Zusammenhang nicht mehr gilt.
 
-#Nummer 3
 
-#Line re, wert a und b für => y' = a * x' + b
-#y = e^b * x^a
-
+#Sie erhalten aus der Regression die Parameter a und b für den Zusammenhang y' = a*x'+b
+#Die Rückrechnung auf den ursprünglichen Zusammenhang geschieht über die Umkehrung
+#der doppelten Logarithmierung: y = e^b * x^a
+#wobei x hier die Spannungsmessung und y die daraus resultierende Abstandsmessung darstellt.
+#Damit haben wir die nichtlineare Kennlinie des Sensors gefunden.
 x = logawerteEingang
 y = logawerteAusgang
-
 (a, b) = np.polyfit(x, y, 1)
 
-#wert a und b für => y' = a * x' + b
-#y = e^b * x^a
-
 yp = np.polyval([a, b], x)
-#plot
+
 fig, ax = plt.subplots()
 ax.plot(x, yp)
 ax.grid(True)
@@ -148,16 +132,12 @@ ax.set_ylabel('Log Abstand [in cm]')
 ax.set_title("a1.pdf Log Kennlinie")
 show()
 
-#Ergebnis
 print("Nichtlineare Kennlinie: y = e^%.3f * x^%.3f" % (b, a))
 
-#newY = pow(e, b) * pow(werte[:,1], a) #Rückrechnung
-#print(newY)
-
-
-#Aufgabe 3
-#Nummer 1
-#DIN A4 PDF a1.pdf
+#3. Flächenmessung mit Fehlerrechnung
+#3.a.1
+#Stellen Sie das Zielobjekt im Abstand eines DIN-A4-Blattes (lange Seite) vom Sensor auf
+#und führen Sie eine einzige Messung aus und speichern Sie das Resultat als csv-Datei
 #Länge: 29,7cm -> 697,6 mV
 #Breite: 21cm -> 877mV
 dina4_breite = np.genfromtxt('../messwerte/dina4b.csv', dtype = float, delimiter = ',', skip_header = 1017, usecols=(4))
@@ -166,93 +146,81 @@ dina4_laenge = np.genfromtxt('../messwerte/dina4l.csv', dtype = float, delimiter
 t_6826 = 1.0 # Faktor t bei Sicherheit P = 68,26%
 t_95 = 1.96 # Faktor t bei Sicherheit P = 95%
 
-discord_breite = 0.6796 #V
-discord_laenge = 0.877 #V
+discord_laenge = 0.6796 #V
+discord_breite = 0.877 #V
+
+#Kennlinien Formel
+def kennlinie(variable_spannung):
+  return (pow(e, b) * pow(variable_spannung, a))
+
+#3.a.2
+#Schätzen Sie den Messfehler nach der Methode aus der Vorlesung. Vergessen Sie
+#dabei nicht eine eventuelle Korrektur aufgrund der Anzahl von Messungen. Geben
+#Sie das Ergebnis Ihrer Spannungsmessung in der korrekten Form an. Wie groß ist der
+#Vertrauensbereich für eine Sicherheit von 68 %, wie groß für eine Sicherheit von 95 %?
+y_meanL = discord_laenge
+y_stdL = np.std(dina4_laenge, dtype=float) # Standardabweichung
+y_empstdL = (y_stdL / sqrt(dina4_laenge.size)) # Empirische Standardabweichung
 
 
-#Nummer 2 BREITE
-#y_meanB = np.mean(dina4_breite, dtype=float)
+print("\n\n")
+
+#3.a.3
+#Geben Sie nun das Ergebnis Ihrer Abstandsmessung in cm in korrekter Form an.
+#Benutzen sie dazu die Fehlerfortpflanzung.
+korrekteAngabe_6826L = (y_meanL + t_6826 * y_empstdL, y_meanL - t_6826 * y_empstdL)
+korrekteAngabe_95L = (y_meanL + t_95 * y_empstdL, y_meanL - t_95 * y_empstdL)
+print(r" x = %f +- %f * %f [V]" % (y_meanL, t_6826, y_empstdL))
+print(r"Messergebnis Laenge mit 68.26%% Sicherheit: %f V bis %f V" % (korrekteAngabe_6826L[0], korrekteAngabe_6826L[1]))
+print(r" x = %f +- %f * %f [V]" % (y_meanL, t_95, y_empstdL))
+print(r"Messergebnis Laenge mit 95%% Sicherheit: %f V bis %f V" % (korrekteAngabe_95L[0], korrekteAngabe_95L[1]))
+
+
+
+#3.b
+#Flächenmessung: Zur Ermittlung der Fläche eines DIN A4-Blattes messen Sie nun nach
+#derselben Methode wie in Aufgabe 3a die Breite des Blattes aus. Berechnen Sie daraus
+#Ihre Schätzung für die Fläche des Blattes und geben Sie Ihr Messergebnis korrekt mit Ihrer
+#Schätzung des Messfehlers an. Benutzen Sie dazu das Gaußsche Fehlerfortpflanzungsgesetz
+#aus der Vorlesung.
+
 y_meanB = discord_breite
-y_stdB = np.std(dina4_breite, dtype=float) # Mittelwert EINZELWERT #1000 Da Volt in mV
-y_empstdB = (y_stdB / sqrt(dina4_breite.size)) # Mittelwert alle Werte
-print("\n\nArithmetisches Mittel Breite Ausgangswerte: %f V" % y_meanB)
-print("Empirische Standardabweichung Breite Ausgangswerte: %f V" % y_empstdB)
+y_stdB = np.std(dina4_breite, dtype=float)
+y_empstdB = (y_stdB / sqrt(dina4_breite.size))
+print("\n\n")
 
 korrekteAngabe_6826B = (y_meanB + t_6826 * y_empstdB, y_meanB - t_6826 * y_empstdB)
 korrekteAngabe_95B = (y_meanB + t_95 * y_empstdB, y_meanB - t_95 * y_empstdB)
-print(r"Messergebnis Breite mit 68.26 Sicherheit: %f V bis %f V" % (korrekteAngabe_6826B[1], korrekteAngabe_6826B[0]))
-print(r"Messergebnis Breite mit 95 Sicherheit: %f V bis %f V" % (korrekteAngabe_95B[1],korrekteAngabe_95B[0]))
+print(r" x = %f +- %f * %f [V]" % (y_meanB, t_6826, y_empstdB))
+print(r"Messergebnis Breite mit 68.26%% Sicherheit: %f V bis %f V" % (korrekteAngabe_6826B[0], korrekteAngabe_6826B[1]))
+print(r" x = %f +- %f * %f [V]" % (y_meanB, t_95, y_empstdB))
+print(r"Messergebnis Breite mit 95%% Sicherheit: %f V bis %f V" % (korrekteAngabe_95B[0],korrekteAngabe_95B[1]))
 
-print("\n")
-#Nummer 2 LÄNGE
-#y_meanL = np.mean(dina4_laenge, dtype=float)
-y_meanL = discord_laenge
-y_stdL = np.std(dina4_laenge, dtype=float) # Mittelwert EINZELWERT 1000, da Volt in mV
-y_empstdL = (y_stdL / sqrt(dina4_laenge.size)) # Mittelwert alle Werte
-print("Arithmetisches Mittel Laenge Ausgangswerte: %f V" % y_meanL)
-print("Empirische Standardabweichung Laenge Ausgangswerte: %f V" % y_empstdL)
+# Fläche = Länge * Breite
 
+#Ableitungsfunktion
+def ableitungkennlinie(variable):
+  return (a * pow(e, b) * pow(variable, a - 1))
 
-korrekteAngabe_6826L = (y_meanL + t_6826 * y_empstdL, y_meanL - t_6826 * y_empstdL)
-korrekteAngabe_95L = (y_meanL + t_95 * y_empstdL, y_meanL - t_95 * y_empstdL)
-print(r"Messergebnis Laenge mit 68.26 Sicherheit: %f V bis %f V" % (korrekteAngabe_6826L[1], korrekteAngabe_6826L[0]))
-print(r"Messergebnis Laenge mit 95 Sicherheit: %f V bis %f V" % (korrekteAngabe_95L[1],korrekteAngabe_95L[0]))
-
-#Nummer 3 Breite
-#Fehlerfortpflanzung
-variable_discord_spannungL = 0.6976
-discordwertL = (pow(e, b) * pow(variable_discord_spannungL, a)) # Kennlinie a1.pdf Discord
-deriativeDiscordL = 3.12093 * pow(variable_discord_spannungL,11.393) #Wolfram Alpha ausgerchnet #21 ist VARIABLE X EINFACH IRGENDEIN CM / Strecke
-deltaXDISCORDL = y_empstdL
-deltaYDISCORDL = deriativeDiscordL * deltaXDISCORDL
-print("Länge - delta Y: %f cm" % deltaYDISCORDL)
-ergebnisDiscordL = (discordwertL + deltaYDISCORDL), (discordwertL - deltaYDISCORDL)
-print("Länge - Ergebnis: %f cm bis %f cm" % (ergebnisDiscordL[1], ergebnisDiscordL[0]))
+deltaXL = y_empstdL
+deltaYL = ableitungkennlinie(discord_laenge) * deltaXL
 
 
-variable_discord_spannungB = 0.877
-discordwertB = (pow(e, b) * pow(variable_discord_spannungB, a)) # Kennlinie a1.pdf Discord
-deriativeDiscordB = 3.12093 * pow(variable_discord_spannungB,11.393) #Wolfram Alpha ausgerchnet #21 ist VARIABLE X EINFACH IRGENDEIN CM / Strecke
+deltaXB = y_empstdB
+deltaYB = ableitungkennlinie(discord_breite) * deltaXB
 
 
+print("\nLänge: x = %f +- %f [cm]" % (kennlinie(discord_laenge), -deltaYL))
+print("Breite: x = %f +- %f [cm]" % (kennlinie(discord_breite), -deltaYB))
 
-deltaXDISCORDB = y_empstdB
-deltaYDISCORDB = deriativeDiscordB * deltaXDISCORDB
+eingang1 = kennlinie(discord_laenge)
+eingang2 = kennlinie(discord_breite)
+# Fläche = eingang1 * eingang2
+ableitungeingang1 = eingang2
+ableitungeingang2 = eingang1
 
-#FINALWERT = Discordwert +- deriative * delta x
-print("Breite - delta Y: %f cm" % deltaYDISCORDB)
-ergebnisDiscordB = (discordwertB + deltaYDISCORDB), (discordwertB - deltaYDISCORDB)
-print("Breite - Ergebnis: %f cm bis %f cm" % (ergebnisDiscordB[1], ergebnisDiscordB[0]))
-
-
-#Partielle Ableitungen
-#Fläche = Länge S * Breite T
-S_discord = discordwertL
-T_discord = discordwertB
-ableitung_S = T_discord
-ableitung_T = S_discord
-delta_S = deltaYDISCORDL
-delta_T = deltaYDISCORDB
-
-delta_V = sqrt(pow(ableitung_S*delta_S, 2) + pow(ableitung_T * delta_T, 2))
+flaecheV = eingang1 * eingang2
+flaecheDeltaV = sqrt(pow(ableitungeingang1 * deltaYL, 2) + pow(ableitungeingang2 * deltaYB, 2))
 
 
-
-#flaecheinsgesamtMIN = ergebnisDiscordB[1] * ergebnisDiscordL[1]
-#flaecheinsgesamtMAX = ergebnisDiscordB[0] * ergebnisDiscordL[0]
-
-ErgebnisMIN = S_discord*T_discord - delta_V
-ErgebnisMAX = S_discord*T_discord + delta_V
-
-print("delta_V: %f" % delta_V)
-print("\nFlaeche - Ergebnis: %f cm^2 bis %f cm^2" % (ErgebnisMIN, ErgebnisMAX))
-originalBlattFlaeche = 29.7 * 21 #Aus a1.pdf abgeschrieben
-print("\nOriginal Flaeche: %f cm^2" % (originalBlattFlaeche))
-
-
-#excelnewY = (pow(e, excelb) * pow(210, excela)) # Kennline Excel Tabellen Moodle
-#deriativeMoodle = 4.36322 * pow(21,7.966) #Wolfram Alpha ausgerchnet #21 ist VARIABLE X EINFACH IRGENDEIN CM / Strecke
-#deltaXMOODLE =
-#print("Laenge: %f V" % excelnewY)
-
-#Nummer 3 b
+print("\nFlaeche Max: x = %f +- %f [cm^2]" % (flaecheV, flaecheDeltaV))
