5dae3116ab5a4c546c0e5116e0504823470cc954
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-05-03 10:53:27 +0200

    Versuch 2

---
+++ Versuch 2/__init__.py
@@ -0,0 +1,30 @@
+import numpy as np
+import imageio
+import matplotlib.pyplot as plt
+import cv2
+
+#Tutorial
+cap = cv2.VideoCapture(0)
+while(True):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break;
+cap.release()
+cv2.destroyAllWindows()
+
+cap = cv2.VideoCapture(0)
+print("frame width: " + str(cap.get(3)))
+print("frame height: " + str(cap.get(4)))
+print("--------------------------------")
+print("brightness: " + str(cap.get(10)))
+print("contrast: " + str(cap.get(11)))
+print("saturation: " + str(cap.get(12)))
+print("--------------------------------")
+print("gain: " + str(cap.get(14)))
+print("exposure: " + str(cap.get(15)))
+print("--------------------------------")
+print("white balance: " + str(cap.get(17)))
+cap.release()
+
