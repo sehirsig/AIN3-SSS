082c628f2ea5b4be3cbaf282f08b249b581dd673
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-05-03 11:29:31 +0200

    Versuch 2

--- Versuch 2/__init__.py
+++ Versuch 2/__init__.py
@@ -4,17 +4,15 @@ import matplotlib.pyplot as plt
 import cv2
 
 #Tutorial
-cap = cv2.VideoCapture(0)
+print("Hello")
+cap = cv2.VideoCapture(-1)
 while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame', gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break;
-cap.release()
-cv2.destroyAllWindows()
 
-cap = cv2.VideoCapture(0)
 print("frame width: " + str(cap.get(3)))
 print("frame height: " + str(cap.get(4)))
 print("--------------------------------")
@@ -27,4 +25,4 @@ print("exposure: " + str(cap.get(15)))
 print("--------------------------------")
 print("white balance: " + str(cap.get(17)))
 cap.release()
-
+cv2.destroyAllWindows()
