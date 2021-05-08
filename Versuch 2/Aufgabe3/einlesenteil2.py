5e0ffa0c895d7da2de95c5fa9414788bc355247f
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-05-08 23:35:13 +0200

    Versuch 2

--- Versuch 2/Aufgabe3/einlesenteil2.py
+++ Versuch 2/Aufgabe3/einlesenteil2.py
@@ -1,22 +1,22 @@
 import cv2
 import numpy as np
 
-imagedb = cv2.imread("../../BilderV2/dunkelbild.png")
+imagedb = cv2.imread("../../BilderV2/dunkelbild.png") #dunkelbild
 imagedb = imagedb.astype('float32')
 
-imagewb = cv2.imread("../../BilderV2/weißbild.png")
+imagewb = cv2.imread("../../BilderV2/weißbild.png") #weißbild
 imagewb = imagewb.astype('float32')
 
-imageub = cv2.imread("../../BilderV2/unbearbeitet.png")
+imageub = cv2.imread("../../BilderV2/unbearbeitet.png") #unbearbeitetes einlesebild
 imageub = imageub.astype('float32')
 
-darksubtract = np.subtract(imageub,imagedb)
+darksubtract = np.subtract(imageub,imagedb) # Dunkelbild von Eingangsbild abziehen
 
-meanwhite = np.norm
+cv2.imwrite("../BilderV2/dunkelbearbeitet.png", darksubtract.astype('uint8'))
 
-fin = np.divide(darksubtract, meanwhite)
+meanwhite = np.divide(imagewb, np.mean(imagewb)) # Weißbild normiert mit Mittelwert zu 1
 
-
+fin = np.divide(darksubtract, meanwhite) # Bearbeitetes Eingangsbild mit meanwhite dividieren
 
 cv2.imwrite("../BilderV2/aufgabe3bearbeitet.png", fin.astype('uint8'))
 
