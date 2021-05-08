332296c08c8be7ca7557eb408c4f7f2101dde76c
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-05-08 23:09:28 +0200

    Versuch 2

---
+++ Versuch 2/einlesenindunkelbild.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+imagedb = cv2.imread("../BilderV2/dunkelbild.png")
+imagedb = imagedb.astype('float32')
+
+imageub = cv2.imread("../BilderV2/unbearbeitet.png")
+imageub = imageub.astype('float32')
+
+fin = np.subtract(imageub,imagedb)
+cv2.imwrite("../BilderV2/dunkelbearbeitet.png", fin.astype('uint8'))
