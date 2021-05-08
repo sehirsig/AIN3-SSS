7ad39129ec3678e7ac8251e49981204e2ebbee1f
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-05-08 21:47:46 +0200

    Versuch 2

--- Versuch 2/aufgabe1.py
+++ Versuch 2/aufgabe1.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 import cv2
 
 image = imageio.imread("../BilderV2/unbearbeitet.png")
-#image = image.astype('float32')
-
+image = image.astype('float32')
 plt.imshow(image)
 plt.show()
 
@@ -15,8 +14,10 @@ grau2beginn = 4000
 grau3beginn = 6000
 weißbeginn = 8000
 
-schwarz = image[schwarzbeginn:grau1beginn - 1]
-grau1 = image[grau1beginn:grau2beginn - 1]
-grau2 = image[grau2beginn:grau3beginn - 1]
-grau3 = image[grau3beginn:weißbeginn - 1]
-weiß = image[weißbeginn:image.size]
+schwarz = image[:,0:50]
+grau1 = image[:,150:200]
+grau2 = image[:,300:350]
+grau3 = image[:,400:450]
+weiß = image[:,550:600]
+
+print(weiß)
