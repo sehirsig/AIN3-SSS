88b4f00e46f50dd33f6818d91e3379dbc00b3832
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-05-08 23:04:42 +0200

    Versuch 2

--- Versuch 2/aufgabe1.py
+++ Versuch 2/aufgabe1.py
@@ -6,29 +6,17 @@ import cv2
 image = cv2.imread("../BilderV2/unbearbeitet.png")
 image = image.astype('float32')
 
-plt.imshow(image)
-plt.show()
-plt.imshow(image/255)
-plt.show()
-
-
-
 schwarz = image[:,0:50]
 grau1 = image[:,150:200]
 grau2 = image[:,300:350]
 grau3 = image[:,400:450]
 weiß = image[:,550:600]
 
-plt.imshow(schwarz/255)
-plt.show()
-plt.imshow(grau1/255)
-plt.show()
-plt.imshow(grau2/255)
-plt.show()
-plt.imshow(grau3/255)
-plt.show()
-plt.imshow(weiß/255)
-plt.show()
+cv2.imwrite("../BilderV2/unterbildschwarz.png", schwarz.astype('uint8'))
+cv2.imwrite("../BilderV2/unterbildgrau1.png", grau1.astype('uint8'))
+cv2.imwrite("../BilderV2/unterbildgrau2.png", grau2.astype('uint8'))
+cv2.imwrite("../BilderV2/unterbildgrau3.png", grau3.astype('uint8'))
+cv2.imwrite("../BilderV2/unterbildweiß.png", weiß.astype('uint8'))
 
 print("Schwarz: STD: %f | Mean: %f" %(np.std(schwarz),np.mean(schwarz)))
 print("Grau1: STD: %f | Mean: %f" %(np.std(grau1),np.mean(grau1)))
