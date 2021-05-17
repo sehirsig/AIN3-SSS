5d9562032e59a70b0c8471a9ab0344dbec99cbd8
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-05-17 13:20:24 +0200

    Versuch 3

--- Versuch 3/__init__.py
+++ Versuch 3/__init__.py
@@ -1,21 +1,9 @@
-import pyaudio
-import numpy
+import numpy as np
 
 import matplotlib.pyplot as plt
-FORMAT = pyaudio.paInt16
-SAMPLEFREQ = 44100
-FRAMESIZE = 1024
-NOFFRAMES = 220
-p = pyaudio.PyAudio()
-print('running')
-stream = p.open(format=FORMAT,channels=1,rate=SAMPLEFREQ,
-input=True,frames_per_buffer=FRAMESIZE)
-data = stream.read(NOFFRAMES*FRAMESIZE)
-decoded = numpy.fromstring(data, 'Int16');
-stream.stop_stream()
-stream.close()
-p.terminate()
-print('done')
-plt.plot(decoded)
-plt.show()
 
+csv_file = "../SoundV3/sound_data.csv"
+
+m1_data = np.genfromtxt(csv_file, dtype = int,usecols=(0))
+plt.plot(m1_data)
+plt.show()
