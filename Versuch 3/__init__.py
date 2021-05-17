9fd4f7d7785cce97f4b5fa5a5388760e3ef20c9e
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-05-17 16:24:54 +0200

    Versuch 3

--- Versuch 3/__init__.py
+++ Versuch 3/__init__.py
@@ -3,15 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 csv_file = "../SoundV3/sound_data.csv"
-data = np.genfromtxt(csv_file, dtype = int,usecols=(0))
+datamicro = np.genfromtxt(csv_file, dtype = int,usecols=(0))
+datamilli = datamicro / 1000
+data = datamilli / 1000 #Volt
 
 
 fouriertransformierte = np.fft.fft(data)
 
 fig, ax = plt.subplots()
 ax.set_xlabel('Abtastpunkte')
-ax.set_ylabel('Amplitude')
-ax.plot(data)
+ax.set_title("Amplitude mit Abtastpunkten")
+fig.text(0.02,0.5,'Amplitude (MilliVolt)', ha='center', va='center', rotation='vertical')
+ax.plot(datamilli)
 
 plt.show()
 
@@ -22,27 +25,44 @@ deltaT = aufnehmsekunden / M #(0.022675736962890623 ms) Abtastintervall
 
 zeitproabtast = []
 for k in range(M):
-    zeitproabtast.append(k*deltaT)
-
-fig3, ax3 = plt.subplots()
-ax3.set_xlabel('Zeit in Sekunden')
-ax3.set_ylabel('Amplitude')
-ax3.plot(zeitproabtast, data)
-
+    zeitproabtast.append(k*deltaT/1000) #ms
+fig4, ax4 = plt.subplots()
+ax4.set_xlabel('Zeit in Sekunden')
+fig4.text(0.02,0.5,'Amplitude (MilliVolt)', ha='center', va='center', rotation='vertical')
+ax4.plot(zeitproabtast, datamilli)
+ax4.set_title("Amplitude mit Aufnahme-Zeit")
 plt.show()
 
 
+
+zeittest = []
+for t in range(300):
+    zeittest.append(t*deltaT)
+fig3, ax3 = plt.subplots()
+ax3.set_xlabel('Zeit in Millisekunden')
+#ax3.set_ylabel('Amplitude')
+fig3.text(0.02,0.5,'Amplitude (MilliVolt)', ha='center', va='center', rotation='vertical')
+ax3.plot(zeittest, datamilli[0:300])
+ax3.set_title("Ausschnitt Grundperiode")
+ax3.grid()
+plt.show()
+
+#GrundPeriode ausrechnen aus Plot:
+# Periode Werte [41.0, 125.0, 209.0, 293.0] * deltaT
+# -> Umgerechnet Wert 1: 0.000930, Wert 2: 0.002834, Wert 3: 0.004739, Wert 4: 0.006644:
+# Wert 2 - 1: 0.001904 . Wert 3 - 2: 0.001905  Wert 4 - 3: 0.0019049999999999996
+# Mittelwert -> 0.001905 Periodendauer (Grundperiode)
+
 #Aufgabe 1
-# Grundperiode: ... miliSekunden
-# Grundfrequenz: ... Hz
+# Grundperiode: 1.905 miliSekunden
+# Grundfrequenz: 1/0.001905 => 524.9343832 Hz
 # Signaldauer M : 5.108390023 Sekunden
 # Abtastfrequenz: 44100 Hz
 # Signallänge M: 225280
 # Abtastintervall deltaT: 0.000022675736962890624 Sekunden
 #
 
-grundfrequenz = np.min(np.abs(fouriertransformierte))
-print(grundfrequenz)
+
 frequencys = []
 for i in range(len(fouriertransformierte)):
     frequencys.append(i / aufnehmsekunden)
@@ -53,4 +73,5 @@ fig2, ax2 = plt.subplots()
 ax2.plot(frequencys, np.abs(fouriertransformierte))
 ax2.set_xlabel('Frequenz (Hz)')
 ax2.set_ylabel('Amplitude (V)')
+ax2.set_title("Fouriertransformierte")
 plt.show()
