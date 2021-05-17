20c65f96ead4a6dc45b3395c8e29dfce3f83b601
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-05-17 14:48:04 +0200

    Versuch 3

--- Versuch 3/__init__.py
+++ Versuch 3/__init__.py
@@ -3,7 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 csv_file = "../SoundV3/sound_data.csv"
+data = np.genfromtxt(csv_file, dtype = int,usecols=(0))
 
-m1_data = np.genfromtxt(csv_file, dtype = int,usecols=(0))
-plt.plot(m1_data)
+
+fouriertransformierte = np.fft.fft(data)
+
+fig, ax = plt.subplots()
+ax.set_xlabel('Abtastpunkte')
+ax.set_ylabel('Frequenz (Hz)')
+ax.plot(data)
+fig2, ax2 = plt.subplots()
+ax2.plot(fouriertransformierte)
 plt.show()
+
+#Grundperiode: 0,00002267573696145125s (1/44100Hz) ??
+#Grundfrequenz: 44100 Hz ??
+#Abtastpunkte: 225280
+#Abtastintervall: 5,10839ms ??
+#
+
+# frequenz = n schwingungen / (Signallänge M) * Abtastintervall
--- Versuch 3/recordAudio.py
+++ Versuch 3/recordAudio.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pyaudio
 import numpy
+import time
 
 import matplotlib.pyplot as plt
 
@@ -10,17 +11,25 @@ FRAMESIZE = 1024
 NOFFRAMES = 220
 
 p = pyaudio.PyAudio()
+
 print('running')
+start = time.perf_counter()
 stream = p.open(format=FORMAT,channels=1,rate=SAMPLEFREQ,input=True,frames_per_buffer=FRAMESIZE)
+
 data = stream.read(NOFFRAMES*FRAMESIZE)
 decoded = numpy.fromstring(data, 'Int16');
+
+end = time.perf_counter()
 stream.stop_stream()
 stream.close()
 p.terminate()
 print('done')
+
+print('time recorded= %f' % (end - start))
+
 plt.plot(decoded)
 plt.show()
 
-csv_file = "../SoundV3/sound_data.csv"
+csv_file = "../SoundV3/sound_data.csv" #Mundharmonika Ton 4
 
 np.savetxt(csv_file, decoded, delimiter=",")
