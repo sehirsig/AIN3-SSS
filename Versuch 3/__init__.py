2d855e1482d1e9371ce938cde3dd2ef164e7aabf
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-05-17 15:45:01 +0200

    Versuch 3

--- Versuch 3/__init__.py
+++ Versuch 3/__init__.py
@@ -10,16 +10,47 @@ fouriertransformierte = np.fft.fft(data)
 
 fig, ax = plt.subplots()
 ax.set_xlabel('Abtastpunkte')
-ax.set_ylabel('Frequenz (Hz)')
+ax.set_ylabel('Amplitude')
 ax.plot(data)
-fig2, ax2 = plt.subplots()
-ax2.plot(fouriertransformierte)
+
 plt.show()
 
-#Grundperiode:
-#Grundfrequenz:
-#Abtastpunkte: 225280
-#Abtastintervall:
+M = 225280 #(Anzahl Abtastungen) / Abtastpunkte / Signallänge
+aufnehmsekunden = 5.108390023 #(s)
+aufnehmmilisekunden = aufnehmsekunden * 1000 #(5108.390023 ms)
+deltaT = aufnehmsekunden / M #(0.022675736962890623 ms) Abtastintervall
+
+zeitproabtast = []
+for k in range(M):
+    zeitproabtast.append(k*deltaT)
+
+fig3, ax3 = plt.subplots()
+ax3.set_xlabel('Zeit in Sekunden')
+ax3.set_ylabel('Amplitude')
+ax3.plot(zeitproabtast, data)
+
+plt.show()
+
+
+#Aufgabe 1
+# Grundperiode: ... miliSekunden
+# Grundfrequenz: ... Hz
+# Signaldauer M : 5.108390023 Sekunden
+# Abtastfrequenz: 44100 Hz
+# Signallänge M: 225280
+# Abtastintervall deltaT: 0.000022675736962890624 Sekunden
 #
 
-# frequenz = n schwingungen / (Signallänge M) * Abtastintervall
+grundfrequenz = np.min(np.abs(fouriertransformierte))
+print(grundfrequenz)
+frequencys = []
+for i in range(len(fouriertransformierte)):
+    frequencys.append(i / aufnehmsekunden)
+
+# n / M * deltaT
+
+fig2, ax2 = plt.subplots()
+ax2.plot(frequencys, np.abs(fouriertransformierte))
+ax2.set_xlabel('Frequenz (Hz)')
+ax2.set_ylabel('Amplitude (V)')
+plt.show()
