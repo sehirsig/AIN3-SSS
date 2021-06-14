7dd2373b16ac69b4f038fb0be6ec29739a63ab95
Sebastian Hirsig <sebastianhirsig@bluewin.ch>
2021-06-14 02:28:06 +0200

    Versuch 4

--- Versuch 4/Aufgabe1/amplitudenspektrum.py
+++ Versuch 4/Aufgabe1/amplitudenspektrum.py
@@ -10,15 +10,16 @@ data = np.genfromtxt(csv_file, dtype = int,usecols=(0))
 fouriertransformierte = np.fft.fft(data)
 aufnehmsekunden = 1 #1 Sekunde
 
+print("da: %s" % np.max(fouriertransformierte))
+
 frequencys = []
 for i in range(len(fouriertransformierte)):
     frequencys.append(i / aufnehmsekunden)
 
-# n / M * deltaT
 
 fig2, ax2 = plt.subplots()
-ax2.plot(frequencys[0:22050], np.abs(fouriertransformierte[0:22050]))
+ax2.plot(frequencys, np.abs(fouriertransformierte))
 ax2.set_xlabel('Frequenz (Hz)')
 ax2.set_ylabel('Amplitude')
-ax2.set_title("Amplitudenspektrum")
+ax2.set_title("Amplitudenspektrum1")
 plt.show()
--- Versuch 4/Aufgabe1/windowing.py
+++ Versuch 4/Aufgabe1/windowing.py
@@ -1,62 +1,35 @@
 import scipy.signal as sgl
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 
 #Aufgabe1 d)
 
 csv_file = "../../SoundV4/sound_data_6942018.csv"
 data = np.genfromtxt(csv_file, dtype = int,usecols=(0))
 
-
-gauss_fenster = np.array(sgl.gaussian(512, 512/4))
-
 # Signal in Abschnitte von 512 Samples
 
+def zerteilen(array):
+    gauss_fenster = np.array(sgl.gaussian(512, 512 / 4))
+    for i in range(0, len(array) - 512 + 1, 256):
+        yield np.concatenate([[0] * i, list(gauss_fenster * array[i:i + 512]), [0] * (len(array) - (i + 512))])
 
-dataabschnitte = []
-tempabschnitt = []
-for i in data:
-    tempabschnitt.append(i)
-    if len(tempabschnitt) == 512:
-        dataabschnitte.append(tempabschnitt)
-        tempabschnitt = tempabschnitt[256:512]
-    if (len(tempabschnitt) == 324) & (len(dataabschnitte) == 171):
-        dataabschnitte.append(tempabschnitt)
-        tempabschnitt = []
+def windowing(array):
+    fenster = np.array(list(zerteilen(data)))
+    return np.fft.fft(fenster).mean(0)
 
-#print('%s'% len(dataabschnitte[171]))
-
-
-
-windows = []
-
-for k in range(len(dataabschnitte)):
-    if k != 171:
-        windows.append(np.multiply(dataabschnitte[k], gauss_fenster))
-        #bearbeitet.append(np.multiply(dataabschnitte[k], gauss_fenster))
-    else:
-        windows.append(np.multiply(dataabschnitte[k], gauss_fenster[0:324]))
-        #bearbeitet.append(np.multiply(dataabschnitte[k], gauss_fenster[0:324]))
-
-#tdd = np.fft.rfft(windows).mean(0)
-print('%s'% windows)
-
-
-
-
+data_transformed = windowing(data)
 
 fouriertransformierte = np.fft.fft(data)
 aufnehmsekunden = 1 #1 Sekunde
 
+frequencys = []
+for i in range(len(fouriertransformierte)):
+    frequencys.append(i / aufnehmsekunden)
 
-
-freqs = np.fft.rfftfreq(len(data), 1 / 44100)
-limit = np.argmax(freqs > 1000)
-
-#fig2, ax2 = plt.subplots()
-#ax2.plot(freqs[1:limit], np.abs(data_fft[1:limit]))
-#ax2.set_xlabel('Frequenz (Hz)')
-#ax2.set_ylabel('Amplitude')
-#ax2.set_title("Amplitudenspektrum")
-#plt.show()
+fig2, ax2 = plt.subplots()
+ax2.plot(frequencys, np.abs(data_transformed))
+ax2.set_xlabel('Frequenz (Hz)')
+ax2.set_ylabel('Amplitude')
+ax2.set_title("Amplitudenspektrum2")
+plt.show()
